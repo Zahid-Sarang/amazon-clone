d1e17b631013ae97da35c97997cf1c2fcf258167
zahid-soft <zahidsarang95@gmail.com>
2021-12-25 22:26:55 +0530

    form ui done

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './Login.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
-    return (
-        <div className='login'>
-        <Link to="/">
-        <img className="login__logo" src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="logo"/>
-        </Link>
-            
-        </div>
-    )
-}
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
+          alt="logo"
+        />
+      </Link>
 
-export default Login
+      <div className="login__container">
+          <h1>Sign-In</h1>
+          <form>
+              <h5>E-mail</h5>
+              <input type="text" placeholder="example@gmail.com" />
+              <h5>Password</h5>
+              <input type="password" placeholder="Enter Correct Password"/>
+              <button type="submit" className="login__sign-in">Sign-In</button>
+          </form>
+          <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice</p>
+          <button className="login__sign-up">Create your Amazon Account</button>
+
+      </div>
+    </div>
+  );
+};
+
+export default Login;
